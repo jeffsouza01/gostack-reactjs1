13ba624c723b40973d06f7d954f52d3480e391d5
Jefferson da Silva <je_jss@hotmail.com>
2020-07-19 04:52:37 -0300

    config api to react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
-import backgroundImage from './assets/background.jpg'
 import Header from './components/Header';
 import './App.css';
 
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento do App', 'Front End Web']);
+    const [projects, setProjects] = useState([]);
 
-    function handleNewProjects () {
+    useEffect(() => {
+        api.get('projects').then( response => {
+            setProjects(response.data);
+        })
+    })
+    
+    async function handleNewProjects () {
         //projects.push(`Novo Projeto ${Date.now()}`);
 
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+        //setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+        const response = await api.post('projects', {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: 'Jefferson da Silva'
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
@@ -21,14 +35,12 @@ function App() {
         
 
         <ul>
-            {projects.map(project => <li key={project}>{project}</li>)}
+            {projects.map(project => <li key={project.id}>{project.title}</li>)}
         </ul>
         
         
         <button type='button' onClick={handleNewProjects}>Adicionar Projeto</button>
 
-
-        <img src={backgroundImage} />
         
         </>
         );
